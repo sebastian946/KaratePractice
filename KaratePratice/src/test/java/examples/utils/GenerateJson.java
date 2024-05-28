68897ded196bf3ef3bbf5df5e6391e965b3b4424
Jhoan Sebastian Duque Zapata <JDUQU13@bancodebogota.com.co>
2024-05-28 11:38:51 -0500

    Cambios iniciales

---
+++ KaratePratice/src/test/java/examples/utils/GenerateJson.java
@@ -0,0 +1,11 @@
+package examples.utils;
+
+import net.datafaker.Faker;
+
+import java.util.List;
+
+public class GenerateJson {
+    static Faker faker = new Faker();
+    static String router = "mapperJsonTest/";
+
+}
